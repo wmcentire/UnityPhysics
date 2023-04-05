2835cfc3ffac8c4833a8ebc43c41c5a744515bf5
wmcentire <wmcentire@student.neumont.edu>
2023-04-04 22:17:33 -0600

    4/4/2023 new jump

--- Assets/Scripts/ControllerCharacter.cs
+++ Assets/Scripts/ControllerCharacter.cs
@@ -1,82 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class ControllerCharacter : MonoBehaviour
 {
-	[SerializeField] float speed;
-	[SerializeField] float turnRate;
-	[SerializeField] float jumpHeight;
-	[SerializeField] float hitForce;
-	[SerializeField] float gravMultiplier;
+    [SerializeField] float speed;
+    [SerializeField] float turnRate;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float doubleJumpHeight;
+    [SerializeField] float hitForce;
+    [SerializeField, Range(1, 5)] float fallRateMultiplier;
+    [Header("Ground")]
+    [SerializeField] Transform groundTransform;
+    [SerializeField] LayerMask groundLayerMask;
+    CharacterController characterController;
+    Vector3 velocity = Vector3.zero;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        // check if the character is on the ground
+        bool onGround = Physics.CheckSphere(groundTransform.position, 0.2f, groundLayerMask, QueryTriggerInteraction.Ignore);
+        // get direction input
+        Vector3 direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        // set velocity
 
-	CharacterController characterController;
-	Vector3 velocity = Vector3.zero;
+        velocity.x = direction.x * speed;
+        velocity.z = direction.z * speed;
 
-	void Start()
-	{
-		characterController = GetComponent<CharacterController>();
-	}
+        if (onGround)
+        {
+            if (velocity.y < 0) velocity.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y += Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y);
+                StartCoroutine(DoubleJump(2.0f));
+            }
+        }
+        // adjust gravity for jump
+        float gravityMultiplier = 1;
+        if (!onGround && velocity.y < 0) gravityMultiplier = fallRateMultiplier;
 
-	void Update()
-	{
-		// get direction input
-		Vector3 direction = Vector3.zero;
-		direction.x = Input.GetAxis("Horizontal");
-		direction.z = Input.GetAxis("Vertical");
+        velocity.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
 
-		// set velocity
-		if (characterController.isGrounded)
-		{
-			velocity.x = direction.x * speed;
-			velocity.z = direction.z * speed;
-			if(velocity.y < 0)
-			{
-				velocity.y = 0;
-			}
-			if (Input.GetButtonDown("Jump"))
-			{
-				velocity.y += Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y * gravMultiplier);
-			}
-		}
+        // move character
+        characterController.Move(velocity * Time.deltaTime);
+        // rotate character to face direction of movement (velocity)
+        Vector3 face = new Vector3(velocity.x, 0, velocity.z);
+        if (face.magnitude > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(face), Time.deltaTime * turnRate);
+        }
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+        // Apply the push
+        body.velocity = pushDir * hitForce;
+    }
 
-		velocity.y += Physics.gravity.y * gravMultiplier * Time.deltaTime;
-
-		// move character
-		characterController.Move(velocity * Time.deltaTime);
-
-		Vector3 face = new Vector3(velocity.x, 0, velocity.z);
-		if(face.magnitude > 0)
-		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(face), Time.deltaTime * turnRate);
-		}
-	}
-
-	void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		Rigidbody body = hit.collider.attachedRigidbody;
-
-		// no rigidbody
-		if (body == null || body.isKinematic)
-		{
-			return;
-		}
-
-		// We dont want to push objects below us
-		if (hit.moveDirection.y < -0.3)
-		{
-			return;
-		}
-
-		// Calculate push direction from move direction,
-		// we only push objects to the sides never up and down
-		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-		// If you know how fast your character is trying to move,
-		// then you can also multiply the push velocity by that.
-
-		// Apply the push
-		body.velocity = pushDir * hitForce;
-	}
+    //corouting
+    IEnumerator DoubleJump(float timer)
+    {
+        // wait a little after the jump to allow a double jump
+        yield return new WaitForSeconds(0.01f);
+        // allow a double jump while moving up
+        while (velocity.y > 0)
+        {
+            // if "jump" pressed add jump velocity
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y += Mathf.Sqrt(doubleJumpHeight * -2 * Physics.gravity.y);
+                break;
+            }
+            yield return null;
+        }
+    }
 }
